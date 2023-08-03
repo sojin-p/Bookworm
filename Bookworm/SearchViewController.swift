6b011bf4781376ac1d9a95b8290f7c09cc0c6457
sojin-p <luv2hong@gmail.com>
2023-08-03 19:34:31 +0900

    검색하면 실시간으로 아웃렛에 표시하는 기능 구현

--- Bookworm/SearchViewController.swift
+++ Bookworm/SearchViewController.swift
@@ -8,15 +8,24 @@
 import UIKit
 
 class SearchViewController: UIViewController {
-
+    
+    let searchBar = UISearchBar()
+    let movieList = MovieInfo().movie
+    var searchList: [String] = []
+    
+    @IBOutlet var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        title = "검색 화면"
         
         let xMarkImage = UIImage(systemName: "xmark")
         navigationItem.leftBarButtonItem = UIBarButtonItem(image: xMarkImage, style: .plain, target: self, action: #selector(closeBarButtonClicked))
         navigationItem.leftBarButtonItem?.tintColor = .black
+        
+        searchBar.delegate = self
+        navigationItem.titleView = searchBar
+        searchBar.placeholder = "영화를 검색해 보세요"
+        searchBar.showsCancelButton = true
         
     }
     
@@ -25,6 +34,26 @@ class SearchViewController: UIViewController {
         dismiss(animated: true)
     }
 
+}
 
+extension SearchViewController: UISearchBarDelegate {
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.text = ""
+        resultLabel.text = ""
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        searchList.removeAll()
+        
+        for movie in movieList {
+            if movie.mainTitle.contains(searchBar.text ?? "") {
+                searchList.append(movie.mainTitle)
+                resultLabel.text = searchList.joined(separator: ", ")
+            } else if let text = searchBar.text, text.isEmpty {
+                resultLabel.text = ""
+            }
+        }
+    }
 }
 
